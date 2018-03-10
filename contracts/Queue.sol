63f0b6c603a59d123e3879447619686e57706cc3
vitali <vshypko@gmail.com>
2018-03-10 13:47:32 -0800

    midterm: Queue done

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -8,52 +8,80 @@ pragma solidity ^0.4.15;
  */
 
 contract Queue {
-	/* State variables */
-	uint8 size = 5;
-	// YOUR CODE HERE
+    /* State variables */
+    uint8 size = 5;
+    uint8 numOfPeople;
+    uint frontTimeLimit;
+    uint frontTimer;
+    address[5] queue;
+    mapping (address => uint8) addressPosition;
 
-	/* Add events */
-	// YOUR CODE HERE
+    /* Add events */
+    event EjectFront(address ejectedAddress);
 
-	/* Add constructor */
-	// YOUR CODE HERE
+    /* Add constructor */
+    function Queue () public {
+        numOfPeople = 0;
+        frontTimeLimit = 5 minutes;
+    }
 
-	/* Returns the number of people waiting in line */
-	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
-	}
+    /* Returns the number of people waiting in line */
+    function qsize() constant returns(uint8) {
+        return numOfPeople;
+    }
 
-	/* Returns whether the queue is empty or not */
-	function empty() constant returns(bool) {
-		// YOUR CODE HERE
-	}
-	
-	/* Returns the address of the person in the front of the queue */
-	function getFirst() constant returns(address) {
-		// YOUR CODE HERE
-	}
-	
-	/* Allows `msg.sender` to check their position in the queue */
-	function checkPlace() constant returns(uint8) {
-		// YOUR CODE HERE
-	}
-	
-	/* Allows anyone to expel the first person in line if their time
-	 * limit is up
-	 */
-	function checkTime() {
-		// YOUR CODE HERE
-	}
-	
-	/* Removes the first person in line; either when their time is up or when
-	 * they are done with their purchase
-	 */
-	function dequeue() {
-		// YOUR CODE HERE
-	}
+    /* Returns whether the queue is empty or not */
+    function empty() constant returns(bool) {
+        return numOfPeople == 0;
+    }
 
-	/* Places `addr` in the first empty position in the queue */
-	function enqueue(address addr) {
-		// YOUR CODE HERE
-	}
+    /* Returns the address of the person in the front of the queue */
+    function getFirst() constant returns(address) {
+        return queue[0];
+    }
+
+    /* Allows `msg.sender` to check their position in the queue */
+    function checkPlace() constant returns(uint8) {
+        return addressPosition[msg.sender];
+    }
+
+    /* Allows anyone to expel the first person in line if their time
+     * limit is up */
+    function checkTime() {
+        address toEject = getFirst();
+        if ((frontTimer + frontTimeLimit) < now) {
+            dequeue();
+        }
+        EjectFront(toEject);
+    }
+
+    /* Removes the first person in line; either when their time is up or when
+    * they are done with their purchase */
+    function dequeue() {
+        require(numOfPeople > 0);
+
+        delete addressPosition[queue[0]];
+        delete queue[0];
+        numOfPeople--;
+
+        for (uint i = 0; i < numOfPeople; i++) {
+            queue[i] = queue[i+1];
+            addressPosition[queue[i]]--;
+        }
+
+        frontTimer = now;
+    }
+
+    /* Places `addr` in the first empty position in the queue */
+    function enqueue(address addr) {
+        require(numOfPeople < size);
+
+        queue[numOfPeople] = addr;
+        addressPosition[addr] = numOfPeople;
+        numOfPeople++;
+
+        if (addressPosition[addr] == 0) {
+            frontTimer = now;
+        }
+    }
 }
