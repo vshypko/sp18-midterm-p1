a351353bec93890177f5e245e1eb81b69870eee0
vitali <vshypko@gmail.com>
2018-03-10 17:03:21 -0800

    midterm: Token done

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.15;
 
 import './interfaces/ERC20Interface.sol';
+import './utils/SafeMath.sol';
 
 /**
  * @title Token
@@ -9,5 +10,92 @@ import './interfaces/ERC20Interface.sol';
  */
 
 contract Token is ERC20Interface {
-	// YOUR CODE HERE
+    using SafeMath for uint256;
+
+    string public constant name = "Midterm Token";
+    string public constant symbol = "MIDT";
+
+    uint256 public totalSupply;
+    uint256 public tokenCap;
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function Token () public {}
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(_value > 0);
+
+        if (balances[msg.sender] >= _value) {
+            balances[msg.sender].sub(_value);
+            balances[_to].add(_value);
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(_value > 0);
+
+        if ((balances[_from] >= _value)
+            && (allowed[_from][msg.sender] >= _value)) {
+            balances[_from].sub(_value);
+            balances[_to].add(_value);
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        if (allowed[msg.sender][_spender] == _value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function burnTokens (uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+
+        if (transfer(address(0), _value)) {
+            totalSupply.sub(_value);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
