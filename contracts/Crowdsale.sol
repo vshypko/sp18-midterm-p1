22e0d1f1508fabfcdc296e6933bdc8eea9fcb983
vitali <vshypko@gmail.com>
2018-03-10 18:39:06 -0800

    midterm: Crowdsale done

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.15;
 
 import './Queue.sol';
 import './Token.sol';
+import './utils/SafeMath.sol';
 
 /**
  * @title Crowdsale
@@ -10,5 +11,78 @@ import './Token.sol';
  */
 
 contract Crowdsale {
-	// YOUR CODE HERE
+    using SafeMath for uint256;
+
+    uint256 tokensSold;
+    uint256 startTime;
+    uint256 endTime;
+    address owner;
+    uint256 initialAmount;
+    uint256 weiWorth;
+
+    Token token;
+    Queue queue;
+
+    event TokenPurchase(address indexed _address, uint256 _value);
+    event TokenRefund(address indexed _address, uint256 _value);
+
+    function Crowdsale () {
+        owner = msg.sender;
+        startTime = now;
+        endTime = now.add(10 days); // ?
+        initialAmount = 1000000;
+        weiWorth = 1;
+        token = new Token();
+        queue = new Queue();
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier saleActive() {
+        require(endTime > now);
+        _;
+    }
+
+    modifier canBuy() {
+        require(msg.sender == queue.getFirst());
+        require(queue.qsize() > 1);
+        _;
+    }
+
+    function mint(uint256 _value) ownerOnly {
+        token.mintTokens(_value);
+    }
+
+    function burn(uint256 _value) ownerOnly {
+        require(token.balanceOf(owner) >= _value);
+
+        token.burnTokens(_value);
+    }
+
+    function buyTokens() payable saleActive canBuy {
+        token.transfer(msg.sender, msg.value.mul(weiWorth));
+        tokensSold.add(msg.value.mul(weiWorth));
+        queue.dequeue();
+        TokenPurchase(msg.sender, msg.value.mul(weiWorth));
+    }
+
+    function refundTokens() payable saleActive {
+        uint256 toRefund = token.balanceOf(msg.sender).div(weiWorth);
+        token.decreaseBalance(msg.sender, token.balanceOf(msg.sender));
+        msg.sender.transfer(toRefund);
+        tokensSold.sub(toRefund);
+    }
+
+    function forwardFunds() ownerOnly returns(bool) {
+        require(now > endTime);
+
+        return owner.send(this.balance);
+    }
+
+    function () payable {
+        revert();
+    }
 }
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -16,18 +16,21 @@ contract Token is ERC20Interface {
     string public constant symbol = "MIDT";
 
     uint256 public totalSupply;
-    uint256 public tokenCap;
+    uint256 public tokenCap; // ?
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function Token () public {}
+    function Token () public {
+        totalSupply = 1000000;
+        balances[msg.sender] = 1000000;
+    }
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) constant returns (uint256) {
         return balances[_owner];
     }
 
@@ -35,7 +38,7 @@ contract Token is ERC20Interface {
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {
+    function transfer(address _to, uint256 _value) returns (bool) {
         require(_value > 0);
 
         if (balances[msg.sender] >= _value) {
@@ -53,7 +56,7 @@ contract Token is ERC20Interface {
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
         require(_value > 0);
 
         if ((balances[_from] >= _value)
@@ -71,7 +74,7 @@ contract Token is ERC20Interface {
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of tokens to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         if (allowed[msg.sender][_spender] == _value) {
@@ -84,11 +87,11 @@ contract Token is ERC20Interface {
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) constant returns (uint256) {
         return allowed[_owner][_spender];
     }
 
-    function burnTokens (uint256 _value) public returns (bool success) {
+    function burnTokens (uint256 _value) public returns (bool) {
         require(balances[msg.sender] >= _value);
 
         if (transfer(address(0), _value)) {
@@ -97,5 +100,13 @@ contract Token is ERC20Interface {
         } else {
             return false;
         }
+    }
+
+    function mintTokens (uint256 _value) {
+        totalSupply.add(_value);
+    }
+
+    function decreaseBalance(address _address, uint256 _value) {
+        balances[_address].sub(_value);
     }
 }
